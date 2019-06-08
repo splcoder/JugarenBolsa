abeef190305f46a7cb4ad02a00d65b21c0b2630a
sergio <www3.splcoder@gmail.com>
2019-06-08 09:40:01 +0200

    todo

--- app/src/main/java/com/example/jugarenbolsa/data/StocksBought.java
+++ app/src/main/java/com/example/jugarenbolsa/data/StocksBought.java
@@ -2,8 +2,9 @@ package com.example.jugarenbolsa.data;
 
 public class StocksBought {
 	public int total = 0;
-	public double spent = 0;
+	public double spent = 0;	// Total spent in this Accion
 	public Accion stock;
+	// TODO array for each accion bought: spent >>> on sell each accion check if wins or not
 
 	public StocksBought(int total, double spent, Accion stock) {
 		this.total = total;

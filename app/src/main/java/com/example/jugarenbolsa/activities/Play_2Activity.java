d15f28e09ef07b4fcf20341c3cf918a094259797
splcoder <www3.splcoder@gmail.com>
2019-06-11 15:04:06 +0200

    imgs...

---
+++ app/src/main/java/com/example/jugarenbolsa/activities/Play_2Activity.java
@@ -0,0 +1,15 @@
+package com.example.jugarenbolsa.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jugarenbolsa.R;
+
+public class Play_2Activity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_play_2);
+	}
+}
